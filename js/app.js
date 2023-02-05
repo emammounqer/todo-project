d8f3a0396dbf1d47948811032559006fbad2102f
Emam Mounqer <23037129@student.ltuc.com>
2023-02-05 12:10:54 +0000

    refactor the user input to match the assigmnet

--- js/app.js
+++ js/app.js
@@ -1,50 +1,32 @@
 'use strict';
 
-class User {
-    name;
-    gender;
-    age;
-
-    constructor() {
-        this.getName()
-        this.getGender();
-        this.getAge();
-
-        if (!confirm('do you want to skip the welcoming message ?'))
-            this.welcoming();
-    }
-
-    getName() {
-        let userName = prompt('what is your name ?');
-        while (userName.length < 1) {
-            userName = prompt('what is your name ? you should enter valid name.');
-        }
-        this.name = userName;
-    }
-
-    getGender() {
-        let userGender = prompt('what is your gender ? male or female ?');
-        while (!(userGender === 'male' || userGender === 'female')) {
-            userGender = prompt('you should enter male or female.');
-        }
-        this.gender = userGender;
-    }
-
-    getAge() {
-        let userAge = Number(prompt('what is your age ?'));
-        while (userAge <= 0 || isNaN(userAge)) {
-            userAge = Number(prompt('your age should be valid number and larger than 0'));
-        }
-        console.log(userAge);
-        this.age = userAge;
-    }
-
-    welcoming() {
-        alert(`hi ${this.gender === 'male' ? 'Mr.' : 'Ms.'} ${this.name}.`);
-    }
-
+let userName = prompt('what is your name ?');
+while (userName.length < 1) {
+    userName = prompt('what is your name ? you should enter valid name.');
 }
 
-const user = new User()
+let userGender = prompt('what is your gender ? male or female ?');
+
+let userAge = Number(prompt('what is your age ?'));
+if (userAge <= 0 || isNaN(userAge)) {
+    alert('your age is not valid number or it less or equal to 0.')
+}
+
+if (!confirm('do you want to skip the welcoming message ?')) {
+    switch (userGender) {
+        case 'male':
+            alert(`hi Mr. ${userName}.`);
+            break
+        case 'female':
+            alert(`hi Ms. ${userName}.`);
+            break;
+        default:
+            alert(`hi ${userName}.`);
+    }
+}
+
+
+
+
 
 
